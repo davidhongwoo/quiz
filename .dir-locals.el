2a9797180e3aef21dd87bc906b682fa616aad3d6
hongwoo <hongwoo7@gmail.com>
2015-10-29 21:49:31 +0900

    add emacs config for indentation.

---
+++ .dir-locals.el
@@ -0,0 +1,16 @@
+((nil
+   (indent-tabs-mode)
+   (tab-width . 4))
+ (c++-mode
+   (c-basic-offset . 4)
+   (indent-tabs-mode . t)
+   (c-file-style . "bsd"))
+ (c-mode
+   (c-basic-offset . 4)
+   (indent-tabs-mode . t)
+   (c-file-style . "bsd")
+   (mode . c++))
+ (c-mode-common
+   (c-default-style . "bsd")
+   (c-fill-style . "bsd")
+   (c-basic-offset 4)))
